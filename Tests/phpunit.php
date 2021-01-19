750a3e4e4aeb4b6fa81893f703ce8f2d2462afba
ve3 <kawewong@gmail.com>
2021-01-19 16:08:19 +0700

    Add required PHP unit test version.

--- Tests/phpunit.php
+++ Tests/phpunit.php
@@ -1,4 +1,13 @@
 <?php
 
 
+$requiredPhpUnitVersion = '7.0';
+if (
+    !class_exists('\\PHPUnit\\Runner\\Version') || 
+    version_compare(\PHPUnit\Runner\Version::id(), $requiredPhpUnitVersion, '<')
+) {
+    die('Required PHPUnit version ' . $requiredPhpUnitVersion);
+}
+
+
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'System' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
